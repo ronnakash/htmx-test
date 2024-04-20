3ecb6ffbba95d15964f1eaf89ab61a5875fe2b89
ronnakash <ronnakash@gmail.com>
2024-04-20 11:35:08 +0300

    filter working

--- js/index.js
+++ js/index.js
@@ -6,12 +6,17 @@ const filterForm = document.getElementById('filterForm');
 // Function to update levelInput field
 function updateLevel(level) {
   levelInput.value = level;
+  updateEvent();
 }
 
 // trigger htmx event on search text change
 search.addEventListener('keyup', function(event) {
   console.log("hi");
   event.preventDefault();
+  updateEvent();
+});
+
+function updateEvent() {
   const updateEvent = new Event("textUpdate");
   filterForm.dispatchEvent(updateEvent);
-});
+}
